687dee546d8774bc5a37eb439b6a70f6e62fc614
nguyenhuyhoang <nguyenhuyhoang241298@gmail.com>
2025-11-26 15:54:57 +0700

    feat(register-captcha): move api to server action register

--- app/(public)/(auth)/register/actions.ts
+++ app/(public)/(auth)/register/actions.ts
@@ -1,26 +1,7 @@
 'use server'
 
-import { env } from '@/env'
 import axiosServer from '@/lib/axios/axiosServer'
-import { validateTurnstileToken } from 'next-turnstile'
-
-export async function checkCaptchaToken(token: string) {
-  if (!token || typeof token !== 'string') {
-    return { error: 'No token provided' }
-  }
-
-  const result = await validateTurnstileToken({
-    token,
-    secretKey: env.TURNSTILE_SECRET_KEY,
-    sandbox: process.env.NODE_ENV === 'development',
-  })
-
-  if (!result.success) {
-    return { error: 'Invalid token' }
-  }
-
-  return { error: null }
-}
+import { checkCaptchaToken } from './helper'
 
 export const registerUser = async (data: {
   email: string
