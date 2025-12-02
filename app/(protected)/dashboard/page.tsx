ab7c131bc884625374af63e49931a72bf5b916b8
nguyenhuyhoang <nguyenhuyhoang241298@gmail.com>
2025-12-02 17:24:06 +0700

    feat(login): init login with email, pass

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,5 +1,28 @@
+'use client'
+
+import axiosClient from '@/lib/axios/axiosClient'
+import { useQuery } from '@tanstack/react-query'
+
 const Page = () => {
-  return <div>Page</div>
+  const userQuery = useQuery({
+    queryKey: ['example'],
+    queryFn: async () => {
+      const res = await axiosClient.get('/api/v1/user')
+      return res.data
+    },
+  })
+
+  if (userQuery.isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (userQuery.isError) {
+    return <div>Error: {(userQuery.error as Error).message}</div>
+  }
+
+  console.log('userQuery.data', userQuery.data)
+
+  return <div>Success</div>
 }
 
 export default Page
