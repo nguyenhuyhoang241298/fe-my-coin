94bd345bfac36f396cb76ecadf04be5343154324
nguyenhuyhoang241298 <nguyenhuyhoang241298@gmail.com>
2025-12-04 23:11:18 +0700

    feat(login): fix samesite

--- auth.ts
+++ auth.ts
@@ -1,7 +1,5 @@
 import NextAuth from 'next-auth'
-import { cookies } from 'next/headers'
 import authConfig from './lib/auth/auth.config'
-import { AUTH_CONFIG } from './lib/auth/configs'
 
 declare module 'next-auth' {
   interface User {
@@ -21,33 +19,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     strategy: 'jwt',
   },
   callbacks: {
-    signIn: async ({ user }) => {
-      const cookieStore = await cookies()
-
-      if (user.accessToken) {
-        cookieStore.set({
-          name: AUTH_CONFIG.ACCESS_TOKEN,
-          value: user.accessToken,
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'none',
-          domain: AUTH_CONFIG.DOMAIN,
-        })
-      }
-
-      if (user.refreshToken) {
-        cookieStore.set({
-          name: AUTH_CONFIG.REFRESH_TOKEN,
-          value: user.refreshToken,
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'none',
-          domain: AUTH_CONFIG.DOMAIN,
-        })
-      }
-
-      return true
-    },
     jwt: async ({ token, user }) => {
       if (user) {
         token.accessToken = user.accessToken
